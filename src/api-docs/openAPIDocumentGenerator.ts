72e77f2ed1ecc157c702407270ea8b1b4020a5bf
aayus karki <karki.aayush2003@icloud.com>
2025-11-26 16:59:39 +0545

    docs(api-docs): update OpenAPI documentation generator

--- src/api-docs/openAPIDocumentGenerator.ts
+++ src/api-docs/openAPIDocumentGenerator.ts
@@ -2,10 +2,11 @@ import { OpenAPIRegistry, OpenApiGeneratorV3 } from "@asteasolutions/zod-to-open
 import { healthCheckRegistry } from "@/api/healthCheck/healthCheckRouter";
 import { userRegistry } from "@/api/user/userRouter";
 import { productRegistry } from "@/api/product/productRouter";
+import { cartRegistry } from "@/api/cart/cartRouter";
 export type OpenAPIDocument = ReturnType<OpenApiGeneratorV3["generateDocument"]>;
 
 export function generateOpenAPIDocument(): OpenAPIDocument {
-	const registry = new OpenAPIRegistry([healthCheckRegistry, userRegistry, productRegistry]);
+	const registry = new OpenAPIRegistry([healthCheckRegistry, userRegistry, productRegistry, cartRegistry]);
 	const generator = new OpenApiGeneratorV3(registry.definitions);
 
 	return generator.generateDocument({
